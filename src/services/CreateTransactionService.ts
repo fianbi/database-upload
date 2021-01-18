704beed1fc8fe619be9c8b465f5c920cacae8c56
Filipe Bittencourt <filipe.bitt10@gmail.com>
2021-01-18 11:26:05 -0300

    Finalizado

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,57 @@
 // import AppError from '../errors/AppError';
 
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'outcome' | 'income';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('Not engough balance');
+    }
+
+    const categoriesRepository = getRepository(Category);
+
+    let findCategory = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!findCategory) {
+      findCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(findCategory);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: findCategory,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
